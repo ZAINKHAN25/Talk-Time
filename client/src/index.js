fdc950570b94a742b56fbbd1ed41063df50517bf
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-07-09 15:51:24 +0500

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/taliwand.css';
+import App from './App';
+import reportWebVitals from "./reportWebVitals.js"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+reportWebVitals();
