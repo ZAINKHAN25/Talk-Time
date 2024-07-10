91cb5ddeee838bdc4a8224db34faf7ac1484a982
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-07-10 16:19:02 +0500

    Imrovement in Joining Section

--- client/src/components/LandingJoiningSections.jsx
+++ client/src/components/LandingJoiningSections.jsx
@@ -17,12 +17,12 @@ function LandingJoiningSections() {
             <input type="text" placeholder="Profile Name" className="w-4/6 p-4 py-2 my-5 rounded-xl transition-all outline-none hover:scale-105 hover:shadow-md" />
 
             <button type="button"
-                className="w-4/6 text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center justify-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mb-5">
+                className="w-4/6  transition-all text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center justify-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mb-5">
                 Quick Start
             </button>
             <div className="flex justify-between items-center w-4/6">
                 <button type="button" className="w-2/4 text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Public Meets</button>
-                <button type="button" className="w-2/4 text-white bg-[#2557D6] hover:bg-[#2567D9]/80 none font-medium rounded-lg text-sm px-5 py-2.5 text-center justify-center inline-flex items-center dark:focus:ring-[#2557D6]/50 me-2 mb-2">
+                <button type="button" className="w-2/4 text-white transition-all bg-[#2567D9]/70 hover:bg-[#2557D6] none font-medium rounded-lg text-sm px-5 py-2.5 text-center justify-center inline-flex items-center dark:focus:ring-[#2557D6]/50 me-2 mb-2">
                     Private Meet
                 </button>
 
