468515200a1f54b68ddca1ef4b2eeb93a4672737
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-07-18 12:01:16 +0500

    Joining Private Meet Component Page ui done.

--- client/src/components/JoiningPrivateMeetComponent.jsx
+++ client/src/components/JoiningPrivateMeetComponent.jsx
@@ -1,12 +1,9 @@
 import React, { useState } from 'react';
 import PrivteJoinImg from "../assets/Private-Join-Img.png";
 import OtpInput from 'react-otp-input';
-
 import { useNavigate } from "react-router-dom";
 
-
 function JoiningPrivateMeetComponent() {
-
     const navigateTo = useNavigate();
     const [otp, setOtp] = useState('');
 
@@ -15,21 +12,16 @@ function JoiningPrivateMeetComponent() {
             <div className='rounded-full bg-gray-700 p-3 sm:p-10'>
                 <img src={PrivteJoinImg} className='w-48 m-2 sm:w-96' />
             </div>
-            <div>
-                <div className='flex items-center flex-col my-2 gap-3'>
 
+            <div className='flex items-center flex-col gap-3'>
+
+                <div className='flex justify-center items-center flex-col gap-3'>
                     <div className='flex flex-col'>
-
-                        {/* <input
-                        type="text"
-                        placeholder="Enter Code"
-                        className="p-4 w-20 py-2 my-5 rounded-xl transition-all outline-none hover:scale-105 hover:shadow-md focus:ring-gray-700 focus:ring-1 text-black"
-                    /> */}
                         <OtpInput
                             containerStyle={"flex"}
-                            inputStyle={"text-black w-10 h-10 my-3 mx-2 text-center font-5xl rounded-xl transition-all outline-none hover:scale-105 hover:shadow-md focus:ring-gray-700 focus:ring-1"}
+                            inputStyle={"text-black w-10 h-10 mx-2 text-center rounded-xl transition-all outline-none hover:scale-105 hover:shadow-md focus:ring-gray-700 focus:ring-1 text-lg"}
                             value={otp}
-                            onChange={setOtp}
+                            onChange={(value) => setOtp(value.toUpperCase())}
                             numInputs={5}
                             placeholder='XXXXX'
                             skipDefaultStyles={true}
@@ -41,18 +33,32 @@ function JoiningPrivateMeetComponent() {
                         onClick={() => navigateTo('/public/zain')}
                         className="transition-all ms-4 text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center justify-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30"
                     >
-                        Join / Create Room
+                        Join
                     </button>
+                    <p className="text-center">
+                        Got an invite code? Enter above.
+                    </p>
 
                 </div>
-                <p
-                    className="text-center"
-                >
-                    Got an invite code? Enter above.
-                </p>
+
+                <div className='flex justify-center items-center flex-col gap-3 w-full'>
+                    <div className='w-full relative text-xl'>
+                        <p className='text-center w-max mx-auto bg-transparent px-2'>Or</p>
+                        <span className='w-5/12 absolute border-b border-black my-auto -translate-y-3 left-0'></span>
+                        <span className='w-5/12 absolute border-b border-black my-auto -translate-y-3 right-0'></span>
+                    </div>
+                    <button
+                        type="button"
+                        onClick={() => navigateTo('/public/zain')}
+                        className="transition-all ms-4 text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center justify-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30"
+                    >
+                        Create a Room
+                    </button>
+                </div>
+
             </div>
-        </div>
-    )
+        </div >
+    );
 }
 
 export default JoiningPrivateMeetComponent;
